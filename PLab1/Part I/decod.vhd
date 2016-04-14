6c1de8589a52746c71f1dd202130d7de9087e561
Mariany Ferreira da Silva <myf@certi.org.br>
2016-04-14 16:15:17 -0300

    change range to input

--- PLab1/Part I/decod.vhd
+++ PLab1/Part I/decod.vhd
@@ -2,21 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity decod is
-port (	inC1, inC2,inC3: in std_logic;
+port (inC1, inC2,inC3: in std_logic;
 		outDecod: out std_logic_vector(6 downto 0)
 	 );
 end decod;
 
-architecture ARCH of Sequenciadores_decod is
+architecture ARCH of decod is
 
 	signal sel : std_logic_vector (2 downto 0);
-
-	sel <= inC1 & inC2 & inC3;
-
+	
 	begin
+	
+		sel <= inC1 & inC2 & inC3;
+	
 	 	outDecod <= "0001001" when sel = "000" else -- H
-					"0000110" when sel = "001" else -- E
-					"1000111" when sel = "010" else -- L
-					"1000000" when sel = "011" else -- O
-					"1111111";
+						"0000110" when sel = "001" else -- E
+						"1000111" when sel = "010" else -- L
+						"1000000" when sel = "011" else -- O
+						"1111111";
 end ARCH;
