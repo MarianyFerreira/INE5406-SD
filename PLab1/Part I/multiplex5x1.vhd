918d9df80593cc6a1128c17c0b18288d252bdfea
Mariany Ferreira da Silva <myf@certi.org.br>
2016-04-14 16:18:27 -0300

    include mux 2x1 5x1

---
+++ PLab1/Part I/multiplex5x1.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity multiplex5x1 is
+    port(
+        input0, input1, input2, input3, input4 : in std_logic_vector(2 downto 0);
+        sel: in std_logic_vector(2 downto 0);
+        output: out std_logic_vector(2 downto 0)
+    );
+end;
+
+architecture arch of multiplex5x1 is
+begin
+    output <= input0 when sel = "000" else 
+				  input1 when sel = "001" else 	
+				  input2 when sel = "010" else 	
+			  	  input3 when sel = "011" else 	
+				  input4;									
+end arch;
