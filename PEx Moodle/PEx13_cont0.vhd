bf24e1471238b919224dcb05f49d4000c6c20717
Mariany Ferreira da Silva <myf@certi.org.br>
2016-04-22 12:43:52 -0300

    update PEx13 Counter leading zeros

--- PEx Moodle/PEx13_cont0.vhd
+++ PEx Moodle/PEx13_cont0.vhd
@@ -7,3 +7,27 @@ entity cont_zeros_esq is
     	outp: out std_logic_vector(2 downto 0)
     );
 end entity;
+
+architecture arch of cont_zeros_esq is
+
+    signal cont std_logic_vector(2 downto 0);
+
+begin
+
+	cont <= "000";
+
+	for indice in 3 downto 0 loop
+		
+		if (inpt(indice) = '0') then
+
+			cont <= std_logic_vector(unsigned(cont) + 1);
+
+		end if;
+	
+	end loop;
+
+	outp <= cont;
+
+and arch;
+
+
