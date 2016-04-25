3580960cd8d53b3a8f184211d7226c2440ab0163
Mariany Ferreira da Silva <MarianyFerreira@users.noreply.github.com>
2016-04-25 19:00:29 -0300

    Update PEx13_cont0.vhd

--- PEx Moodle/PEx13_cont0.vhd
+++ PEx Moodle/PEx13_cont0.vhd
@@ -11,24 +11,24 @@ end entity;
 architecture arch of cont_zeros_esq is
 
     signal cont : std_logic_vector(2 downto 0) := "000";		-- Signal pra contar quantos zeros a esquerda tem
-    signal inputS: std_logic_vector(3 downto 0);				-- Signal que recebe a entrada inpt
+    signal inputS: std_logic_vector(3 downto 0);			-- Signal que recebe a entrada inpt
 
 
 begin
 
-	inputS <= inpt;												-- atribuição do inpt no int
+	inputS <= inpt;							-- atribuição do inpt no int
 
-	for indice in 3 downto 0 loop								-- for que decrementa indice = 3 >>> 0
+	for indice in 3 downto 0 loop					-- for que decrementa indice = 3 >>> 0
 		
-		if (inputS(indice) = '0') then							-- se o vator no indice (3, 2, 1, 0) tiver zeros
+		if (inputS(indice) = '0') then				-- se o vator no indice (3, 2, 1, 0) tiver zeros
 
-			cont <= std_logic_vector(unsigned(cont) + 1);		-- soma 1 ao cont
+			cont <= std_logic_vector(unsigned(cont) + 1);	-- soma 1 ao cont
 
 		end if;
 	
 	end loop;
 
-	outp <= cont;												-- saida recebe o valor da quantidade de zeros
+	outp <= cont;							-- saida recebe o valor da quantidade de zeros
 
 and arch;
 
