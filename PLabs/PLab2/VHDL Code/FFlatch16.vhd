b8ff5e13278de995ba91970eca139c453d24d4de
Mariany Ferreira da Silva <MarianyFerreira@users.noreply.github.com>
2016-06-02 13:58:38 -0300

    Update FFlatch16.vhd

--- PLabs/PLab2/VHDL Code/FFlatch16.vhd
+++ PLabs/PLab2/VHDL Code/FFlatch16.vhd
@@ -1,21 +1,14 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all;
 
-<<<<<<< HEAD:PLabs/PLab2/VHDL Code/FFlatch16.vhd
-ENTITY FFlatch16 IS
-=======
 ENTITY FFlatch16 is
->>>>>>> 1292df4ddb1b9d0e11ed404b35ffa2820a3a0d67:PLabs/PLab2/VHDL Code/FFlatch__16bits.vhd
+
 	PORT ( 	RESET, CLK 	: IN STD_LOGIC;
 			D : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
 
 			Q : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
 		 );
-<<<<<<< HEAD:PLabs/PLab2/VHDL Code/FFlatch16.vhd
 END FFlatch16 ;
-=======
-END FFlatch16;
->>>>>>> 1292df4ddb1b9d0e11ed404b35ffa2820a3a0d67:PLabs/PLab2/VHDL Code/FFlatch__16bits.vhd
 
 ARCHITECTURE Behavior OF FFlatch16 IS
 
